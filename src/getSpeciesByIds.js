39d723a52353989a0834819d8c62626f7a460443
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-29 18:15:08 -0300

    Primeira task

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data
+
+const getSpeciesByIds = (...ids) => {
+    if (ids === undefined) return [];{
+     return species.filter((id ) => ids.includes(id.id))
+    }
 }
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
