92395655dc720fddc101aa7219d72a8c72ece958
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-29 19:54:52 -0300

    Arrumando o lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const oi = species.find((ni) => ni.name === animal);
+  if (oi.residents[0].age > age) {
+    return true;
+  } return false;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,10 @@
 const data = require('../data/zoo_data');
 
-const { species } = data
+const { species } = data;
 
 const getSpeciesByIds = (...ids) => {
-    if (ids === undefined) return [];{
-     return species.filter((id ) => ids.includes(id.id))
-    }
-}
-
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+  if (ids === undefined) return [];
+  return species.filter((id) => ids.includes(id.id));
+};
 
 module.exports = getSpeciesByIds;
