942b3d2fda3e7eac7f8189fb42220bb773dc457c
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-30 19:05:50 -0300

    Task 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+function conut() {
+  return species.reduce((acc, spe) => {
+    const num = acc;
+    num[spe.name] = spe.residents.length;
+    return num;
+  }, {});
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return conut();
+  }
+  const res = species.find((specie) => specie.name === animal.specie).residents;
+  if (!animal.sex) {
+    return res.length;
+  }
+  return res.reduce((soma, residente) => {
+    let s = soma;
+    if (residente.sex === animal.sex) {
+      s += 1;
+    }
+    return s;
+  }, 0);
 }
 
 module.exports = countAnimals;
