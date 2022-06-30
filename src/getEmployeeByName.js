505a7e4e1d47d016c12f4445c0ced5784a1d4acb
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-29 22:38:53 -0300

    Terceira task concluida com os linsts ok

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+// const { species, employees } = data;
 
+function getEmployeeByName(employeeName) {
+  if (employeeName === undefined) return {};
+  const getName = data.employees.find((procurar) =>
+    procurar.firstName === employeeName || procurar.lastName === employeeName);
+  return getName;
+}
+// console.log(getEmployeeByName('Burl'));
 module.exports = getEmployeeByName;
