c5e9aa6f75654294fa4b3e971f6e80a06bda719d
Samuel Hyrum <samuelhymp@gmail.com>
2022-06-30 13:40:47 -0300

    Task 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const verify = data.employees.some((get) => get.managers[0] === id || get.managers[1] === id);
+  return verify;
 }
-
+// console.log(isManager('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter((get) =>
+      get.managers[0] === managerId).map((get) => `${get.firstName} ${get.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
