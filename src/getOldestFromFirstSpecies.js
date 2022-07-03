25f2119e342c569e3f19c7b07936bc5c83420f12
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-03 10:39:30 -0300

    Task numero 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { species } = data;
 
+function getOldestFromFirstSpecies(id) {
+  const result = data.employees.filter((colab) => colab.id === id)
+    .map((animalOld) => animalOld.responsibleFor[0]);
+  const array = species.filter((ani) => ani.id === result[0]);
+  const map = array.map((anim) => anim.residents);
+  const maxAge = map.reduce((a, b) => (a.age > b.age ? a : b));
+  const sort = maxAge.sort((a, b) => b.age - a.age);
+
+  return Object.values(sort[0]);
+}
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
-
+// const {data1} = data;
+// const obj = Object.keys(data1);
+// console.log(obj);
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+//   return data.hours.reduce((acc, days) => {
+//     acc[days] = {'officeHour': 'Open from 8am until 6pm,'}
+//     // acc[days] = {'exhibition': 'Open from 8am until 6pm',}
+//     return acc
+//   }, {})
 }
+// console.log(getSchedule());
+
+// return getAnimal(scheduleTarget)
+// function getAnimal(animal) {
+//   const ni = data.species.find((an) => an.name === animal)
+//   if (!ni) {
+//     return false;
+//   } return ni.availability;
+// }
+// console.log(getSchedule('lions'));
 
 module.exports = getSchedule;
