2e6db7b6197f8bede9b038c1d764b34e8cc29e02
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-05 13:08:19 -0300

    Task numero 11 concluida com lint ok

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+function getEmployees() {
+  return employees.map((cola) => ({
+    id: cola.id,
+    fullName: `${cola.firstName} ${cola.lastName}`,
+    species: cola.responsibleFor.map((IdAnimal) => species.find((specie) =>
+      specie.id === IdAnimal).name),
+    locations: cola.responsibleFor.map((IdAnimal) => species.find((specie) =>
+      specie.id === IdAnimal).location),
+  }));
+}
+
+function getEmployeesCoverage(IdP) {
+  if (IdP === undefined) {
+    return getEmployees();
+  } const result = employees.filter((colab) => colab.firstName === IdP.name
+  || colab.lastName === IdP.name
+  || colab.id === IdP.id).map((id) => id.id);
+  if (result.length === 0) {
+    throw new Error('Informações inválidas');
+  } if (result.length !== 0) {
+    return getEmployees().find((info) => info.id === result[0]);
+  }
 }
 
 module.exports = getEmployeesCoverage;
